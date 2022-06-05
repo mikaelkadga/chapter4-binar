a4054cf6b8038156112209eae0e064b49e7e4049
mikaelkadga <mikaelkadga@gmail.com>
2022-06-05 15:47:52 +0700

    task two

--- task-2/script.js
+++ task-2/script.js
@@ -1,9 +1,12 @@
-const boxElements = document.querySelectorAll('.box')
+let boxElements = document.querySelectorAll('.box')
 const btn = document.getElementsByTagName('button')[0]
 let turn = "x"
+boxElements = Array.from(boxElements)
+
 
 start();
 btn.addEventListener('click', start)
+
 
 function start() {
     turn = "x"
@@ -22,26 +25,6 @@ function handleClick() {
     })
 }
 
-function findWinner() {
-    const winningCombination = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ]
-
-    for (const combination of winningCombination) {
-        const [a, b, c] = combination;
-
-        if(box[a] && (box[a]===box[b] && box[a]=== box[c])) {
-            alert(`${box[a]} is a winner`)
-        }
-    }
     
-}
 
 
