327d4e3b819b1c339ea052af195efda75e803f25
mikael <mikael-pc@iMac.local>
2022-06-06 15:27:01 +0700

    task 2

--- task-2/script.js
+++ task-2/script.js
@@ -8,6 +8,25 @@ start();
 btn.addEventListener('click', start)
 
 
+const winningCombination = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [0,4,8]
+]
+
+function findWinner() {
+    return winningCombination.some(combination => {
+        return combination.every(index => {
+            return(boxElements[index].innerText.trim() === turn)
+        })
+    })
+}
+
 function start() {
     turn = "x"
     boxElements.forEach((box) => {
@@ -20,6 +39,9 @@ function handleClick() {
     boxElements.forEach((box) => {
         box.addEventListener("click", function() {
             box.innerText = turn
+            if(findWinner()){
+                console.log(`${turn} won`)
+            }
             turn = turn === "x" ? "o" : "x"
         }, {once: true})
     })
